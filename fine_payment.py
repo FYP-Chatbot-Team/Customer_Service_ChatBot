01df79ef724899b02b0b023a8ba3679fef476fac
19046836 <19046836@myrp.edu.sg>
2021-11-18 08:54:17 +0800

    UPDATE: Fixing "AttributeError: __enter__" Error 1.0 - Syabil

--- fine_payment.py
+++ fine_payment.py
@@ -9,33 +9,16 @@ import mysql.connector
 #Import from other module
 import database
 
-#setup connection to mySQL database
-#function for DB connection
-def create_connection():
-    """ create a database connection to the mySQL database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    try:
-        conn = mydb = mysql.connector.connect(
-    host="us-cdbr-east-04.cleardb.com",
-    user="b33cdae453531f",
-    password = "6a5d5f6d",
-    database="heroku_edda316505ad5d8"
-    )
-        return conn
-    except Error as e:
-        print(e)
-    return None
 
 
+
+#To test webhook connection
 #How to pay fine function
 def how_to_pay_fine(data):
     reply = {}
     msgs = []
-    conn = create_connection()
-    with conn:
+    conn = database.create_connection()
+    with conn():
           msgs.append({"text": {"text":["You can pay using the following option : "]}})
           cur = conn.cursor()
           cur.execute("SELECT response_1,response_2,response_3,response_4 FROM question where question_id = 1")
@@ -53,8 +36,8 @@ def how_to_pay_fine(data):
 def how_to_appeal(data):
     reply = {}
     msgs = []
-    conn = create_connection()
-    with conn:
+    conn = database.create_connection()
+    with conn():
           cur = conn.cursor()
           msgs.append({"text": {"text":["How to appeal ? Check the following scenario : "]}})
           cur.execute("SELECT response_1 FROM question where question_id = 2")
@@ -72,8 +55,8 @@ def how_to_appeal(data):
 def missed_court_date(data):
     reply = {}
     msgs = []
-    conn = create_connection()
-    with conn:
+    conn = database.create_connection()
+    with conn():
           cur = conn.cursor()
           msgs.append({"text": {"text":["Missed your court date ? Check the following scenario : "]}})
           cur.execute("SELECT response_1,response_2,response_3,response_4,response_5 FROM question where question_id = 3")
@@ -91,8 +74,8 @@ def missed_court_date(data):
 def court_attendance_status(data):
     reply = {}
     msgs = []
-    conn = create_connection()
-    with conn:
+    conn = database.create_connection()
+    with conn():
           cur = conn.cursor()
           msgs.append({"text": {"text":["Court attendance required after fine payment ? Check the following scenario : "]}})
           cur.execute("SELECT response_1,response_2,response_3 FROM question where question_id = 4")
