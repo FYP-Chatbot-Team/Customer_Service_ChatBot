388c10dfa8a4fa7414430707766e436e01a43589
syalabil <syabil125@gmail.com>
2021-12-24 23:43:23 +0800

    Update: Update Firebase 3.0 - Syabil

--- fine_payment.py
+++ fine_payment.py
@@ -35,7 +35,7 @@ def how_to_pay_fine(data):
     ref = db.reference("/Questions/Qn1/")
 
 
-    row = ref.get()
+    rows = ref.get()
     for row in rows:
           for response in row:
                    msgs.append({"text": {"text":[response]}})
